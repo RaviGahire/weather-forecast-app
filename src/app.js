04e8a77e7982724ba3a0ff2587e17bbf7d51cefa
RaviGahire <ravigahire3@gmail.com>
2025-09-13 00:22:48 +0530

    Added a scroll to header and fixed some css

--- src/app.js
+++ src/app.js
@@ -4,12 +4,24 @@ const hamburgerBtn = document.querySelector(".hamburger-menu-icon");
 const closeBtn = document.querySelector(".close-menu-btn");
 const navGroup = document.querySelector(".nav-group");
 
+
 // Open menu
 hamburgerBtn.addEventListener("click", () => {
-  navGroup.classList.add("active");
+    navGroup.classList.add("active");
 });
 
 // Close menu
 closeBtn.addEventListener("click", () => {
-  navGroup.classList.remove("active");
+    navGroup.classList.remove("active");
+});
+
+// Sticky Navbar
+const header = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 40) {
+        header.classList.add('header-small');
+        navGroup.classList.remove("active");
+    } else {
+        header.classList.remove('header-small');
+    }
 });
