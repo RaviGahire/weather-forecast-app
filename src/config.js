8ecadf46e8f5dc39ac6ed1f08ed6a0b628d6b15a
RaviGahire <ravigahire3@gmail.com>
2025-09-29 21:47:58 +0530

    removed files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-
-
-const ConfigKeys = {
-    OPENWEATHER_KEY: "849738ce822301c80e510c5bd4b40bd4",
-    OPENAI_KEY: "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"
-}
-
-export default ConfigKeys;
